83b2cf95686a4ae1420fc8bdda1e1b7617e0fed9
Gulzada Bolotova <64300098+GulzadaBolotova@users.noreply.github.com>
2020-05-17 16:04:18 +0600

    Use state to store items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import NewItem from "./NewItem/NewItem";
 import Items from "./Items/Items";
 import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([
+    { text: "Finish homework", completed: true },
+    { text: "Cook a dinner", completed: false },
+    { text: "Fix the car", completed: true },
+    { text: "Do the laundry", completed: false }
+  ]);
+
   return (
     <div className="App">
       <NewItem />
-      <Items />
+      <Items items={items} />
     </div>
   );
 }
--- src/Items/Items.js
+++ src/Items/Items.js
@@ -2,14 +2,8 @@ import React from "react";
 import Item from "./Item/Item";
 import "./Items.css";
 
-function Items() {
-  const items = [
-    { text: "Finish homework", completed: true },
-    { text: "Cook a dinner", completed: false },
-    { text: "Fix the car", completed: true },
-    { text: "Do the laundry", completed: false }
-  ];
-  const itemsOutput = items.map(item => (
+function Items(props) {
+  const itemsOutput = props.items.map(item => (
     <Item completed={item.completed}>{item.text}</Item>
   ));
   return <ul className="Items">{itemsOutput}</ul>;
